f349edf4e38066b2962420029bdd3b9e6f9e4749
simplyCyclops <maipez04@gmail.com>
2020-08-10 11:42:01 +0300

    yes

--- line.py
+++ line.py
@@ -1,33 +1,33 @@
 from robot import Robot
 
-  black = 10 #needs to be calibrated
+black = 10 #needs to be calibrated
 
-  def align(drive_speed, align_speed):
-    right_value, left_value, on_line, aligned = calc_line()
+def align(drive_speed, align_speed):
+  right_value, left_value, on_line, aligned = calc_line()
 
-    if(not on_line):
-      robot.chassis.drive(drive_speed, 0)
-      while(not on_line):
-        right_value, left_value, on_line, aligned = calc_line()
-      robot.chassis.stop()
-      robot.brake()
-        
-    if(not aligned):
-      if(right_value < black):
-        robot.wheel_left.run(align_speed)
-      elif(left_value < black):
-        robot.wheel_right.run(align_speed)
+  if(not on_line):
+    Robot.chassis.drive(drive_speed, 0)
+    while(not on_line):
+      right_value, left_value, on_line, aligned = calc_line()
+    Robot.chassis.stop()
+    Robot.brake()
+      
+  if(not aligned):
+    if(right_value < black):
+      Robot.wheel_left.run(align_speed)
+    elif(left_value < black):
+      Robot.wheel_right.run(align_speed)
 
-      while(not aligned):
-        right_value, left_value, on_line, aligned = calc_line()
-        robot.brick.screen.print(left_value, right_value)      
-      robot.brake()
+    while(not aligned):
+      right_value, left_value, on_line, aligned = calc_line()
+      Robot.brick.screen.print(left_value, right_value)      
+    Robot.brake()
 
-  def calc_line():
-    right_value = robot.color_right.reflection()
-    reft_value = robot.color_left.reflection()
+def calc_line():
+  right_value = Robot.color_right.reflection()
+  left_value = Robot.color_left.reflection()
 
-    on_line = right_value < black or left_value < black
-    aligned = right_value < black and left_value < black
+  on_line = right_value < black or left_value < black
+  aligned = right_value < black and left_value < black
 
-    return (right_value, left_value, on_line, aligned)
+  return (right_value, left_value, on_line, aligned)
