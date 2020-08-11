5701ffbbd93bdeabad2394c95c01b4490eb4a8bb
simplyCyclops <maipez04@gmail.com>
2020-08-11 12:36:37 +0300

    simplefied input

--- util/input.py
+++ util/input.py
@@ -2,48 +2,23 @@ from pybricks.parameters import Button
 from util.robot import Robot
 import time
 
-lastPressed = {
-  Button.UP: time.time(),
-  Button.RIGHT: time.time(),
-  Button.DOWN: time.time(),
-  Button.LEFT: time.time(),
-  Button.CENTER: time.time()
-}
-press_time = 0.7
-
-def button(btn):
+def button_pressed(btn):
   return btn in Robot.brick.buttons.pressed()
 
-def button_down(btn, refreshTime = 0.35):
-  if(lastPressed[btn] + refreshTime <= time.time()):
-    lastPressed[btn] = time.time()
-    return button(btn)
-
-  return False
-
 def wait_for_press(btn):
-  while True:
-    while(not button(btn)):
-      pass
+  while(not button_pressed(btn)):
+    pass
 
-    start_time = time.time()
-
-    while(button(btn)):
-      pass
-    
-    if(time.time() < start_time + press_time):
-      return
+  while(button_pressed(btn)):
+    pass
 
 def wait_for_any_press():
-  while True:
-    while(not len(Robot.brick.buttons.pressed()) == 1):
-      pass
+  while(not len(Robot.brick.buttons.pressed()) == 1):
+    pass
 
-    start_time = time.time()
-    btn = Robot.brick.buttons.pressed()[0]
+  btn = Robot.brick.buttons.pressed()[0]
 
-    while(len(Robot.brick.buttons.pressed()) > 0):
-      pass
+  while(button_pressed(btn)):
+    pass
 
-    if(time.time() < start_time + press_time):
-      return btn
+  return btn
